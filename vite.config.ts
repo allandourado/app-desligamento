e3184aea2b596a37c10b43e23989db96efdb240d
allandourado <allandourado@meirelesefreitas.com.br>
2026-05-03 14:06:49 -0300

    Upload vite.config.ts via GitHub File Manager

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+export default defineConfig({
+  root: './',
+  plugins: [viteSingleFile()],
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 100000000, // Inline everything
+  },
+})
